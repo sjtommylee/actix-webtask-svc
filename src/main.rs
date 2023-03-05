f03116901f2ebb40250c64ae06b5628e72700662
sjtommylee <sjtommylee@gmail.com>
2023-03-05 15:28:20 -0800

    http server struct, adding logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+
+use api::task:: {
+    get_task
 }
+
+use actix_web::{HTTPServer, App, web::Data, middleware::Logger};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    HTTPServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .data(Data::new(get_task()))
+    })
+}
+
